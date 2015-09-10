975cb6e784b0c3053ae2dc3d60e8a2eb8fe71ad2
Tim Bauman <tebauman@gmail.com>
2015-09-09 22:13:52 -0700

    Updated Resume, fixed Contact container, re-worded skills blurb

--- js/animate.js
+++ js/animate.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
