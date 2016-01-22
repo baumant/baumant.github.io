fd1e0bf56c017f2eafd36d6b506cd3cd67a256a9
Timothy Bauman <tebauman@gmail.com>
2016-01-22 12:29:34 -0800

    Diamond Images
    Diamond Images

--- test/test.js
+++
@@ -1,6 +0,0 @@
-var images = [];
-for (var i = 35; i < 60; i++) {
-	images.push("image-0"+i+".png");
-}
-console.log(images);
-$("img").threesixty({images:images, method:'mousemove', direction:'forward', sensibility: 1});
